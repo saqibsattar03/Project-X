dd5a5d0f03d25975beabea01bd237ec7cfa726d8
saqibsattar03 <ranasaqib734@gmail.com>
2022-04-06 10:28:17 +0500

    player_controller updating...

--- Project-X/Assets/Scripts/PlayerController.cs
+++ Project-X/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
 
-    [SerializeField] private float playerSpeed = 2.0f;
+   // [SerializeField] private float playerSpeed = 2.0f;
 
     private Rigidbody playerRb;
+    public Animator anim;
+    public bool isGrounded;
+    public LayerMask layerMask;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,39 +16,80 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {   
-        PlayerMovement();
-    }
+	private void Update()
+	{
+		Grounded();
+		Jump();
+		Move();
+	}
 
-    protected void PlayerMovement() 
-    {
-        if (Input.GetKey(KeyCode.W))
-        {
-            Debug.Log("w pressed");
-            playerRb.AddForce(Vector3.forward * Time.deltaTime * playerSpeed, ForceMode.Impulse);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            Debug.Log("s pressed");
-            playerRb.AddForce(Vector3.back * Time.deltaTime * playerSpeed, ForceMode.Impulse);
-        }
+	//protected void PlayerMovement() 
+	//{
+	//    if (Input.GetKey(KeyCode.W))
+	//    {
+	//        Debug.Log("w pressed");
+	//        playerRb.AddForce(Vector3.forward * Time.deltaTime * playerSpeed, ForceMode.Impulse);
+	//    }
+	//    else if (Input.GetKey(KeyCode.S))
+	//    {
+	//        Debug.Log("s pressed");
+	//        playerRb.AddForce(Vector3.back * Time.deltaTime * playerSpeed, ForceMode.Impulse);
+	//    }
 
-        else if (Input.GetKey(KeyCode.A))
-        {
-            Debug.Log("a pressed");
-            playerRb.AddForce(Vector3.left * Time.deltaTime * playerSpeed, ForceMode.Impulse);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            Debug.Log("d pressed");
-            playerRb.AddForce(Vector3.right * Time.deltaTime * playerSpeed, ForceMode.Impulse);
-        }
-    }
+	//    else if (Input.GetKey(KeyCode.A))
+	//    {
+	//        Debug.Log("a pressed");
+	//        playerRb.AddForce(Vector3.left * Time.deltaTime * playerSpeed, ForceMode.Impulse);
+	//    }
+	//    else if (Input.GetKey(KeyCode.D))
+	//    {
+	//        Debug.Log("d pressed");
+	//        playerRb.AddForce(Vector3.right * Time.deltaTime * playerSpeed, ForceMode.Impulse);
+	//    }
+	//}
 
-    private void AimTowardMouse()
+	private void AimTowardMouse()
     {
 		float mouseX = Input.GetAxis("Mouse X") * 150 * Time.deltaTime;
 		transform.Rotate(Vector3.up, mouseX);
+	}
+
+	protected void Jump() 
+	{
+		if (Input.GetKey(KeyCode.Space) && isGrounded) 
+		{
+			playerRb.AddForce(Vector3.up * 2, ForceMode.Impulse);
+		}
+	}
+
+	protected void Grounded()
+	{
+		if (Physics.CheckSphere(transform.position + Vector3.down, 0.2f, layerMask))
+		{
+
+			isGrounded = true;
+			Debug.Log("grounded " + isGrounded);
+		}
+		else 
+		{
+			isGrounded = false;
+			Debug.Log("grounded " + isGrounded);
+		}
+			
+		anim.SetBool("jump", !isGrounded);
+	}
+
+	protected void Move() 
+	{
+		float verticalAxis = Input.GetAxis("Vertical");
+		float horizontalAxis = Input.GetAxis("Horizontal");
+
+		Vector3 movement = transform.forward * verticalAxis + transform.right * horizontalAxis;
+		movement.Normalize();
+
+		transform.position += movement * 0.04f;
+
+		anim.SetFloat("vertical", verticalAxis);
+		anim.SetFloat("horizontal",horizontalAxis);
 	}
 }
